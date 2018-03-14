3ff7c51aaad8a2843a9f16b3a5e1fb7656b95849
ntzora <natasatzora21@gmail.com>
2018-03-14 21:03:50 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -5,7 +5,7 @@ var nodes = [];
 var force, node, data, maxVal;
 var brake = 0.2;
 var radius = d3.scale.sqrt().range([10, 20]);
-var sound = new Audio("beep-07.mp3");
+var beep = new Audio("beep-07.mp3");
 
 var partyCentres = { 
     con: { x: w / 3, y: h / 3.3}, 
@@ -44,6 +44,7 @@ var comma = d3.format(",.0f");
 
 function transition(name) {
 	if (name === "all-donations") {
+		beep.play();
 		$("#initial-content").fadeIn(250);
 		$("#value-scale").fadeIn(1000);
 		$("#view-donor-type").fadeOut(250);
